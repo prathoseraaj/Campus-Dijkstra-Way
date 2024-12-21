439c3e9a8ca2d41944129a77ae7cf0b2714270ae
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-12-21 22:52:29 +0530

    feat: added the value to the graph

--- dijkstraalgo.py
+++ dijkstraalgo.py
@@ -38,15 +38,27 @@ def shortest_path(previous_nodes,end):
 
 
 graph = {
-        'A': {'B': 1, 'C': 4},
-        'B': {'A': 1, 'C': 2, 'D': 5},
-        'C': {'A': 4, 'B': 2, 'D': 1, 'E': 3},
-        'D': {'B': 5, 'C': 1, 'E': 2},
-        'E': {'C': 3, 'D': 2}
-    }
+    'Jain University Gate': {'Golf course': 100, 'Short-cut near Non IT building': 350},
+    'Golf course': {'Jain University Gate': 100, 'FET (MAIN)': 300},
+    'FET (MAIN)': {'Golf course': 300, 'FET (BACK)': 97},
+    'FET (BACK)': {'FET (MAIN)': 97, 'Mess': 44, 'Himalayas': 63},
+    'Himalayas': {'FET (BACK)': 63, 'Mess': 71},
+    'Mess': {'FET (BACK)': 44, 'Himalayas': 71, 'Purvanchal': 71},
+    'Purvanchal': {'Mess': 71, 'Karakoram': 39},
+    'Karakoram': {'Purvanchal': 39, 'Football field': 160},
+    'Football field': {'Karakoram': 160, 'Tennis court': 98},
+    'Tennis court': {'Football field': 98, 'Sprintoor': 180, 'Sai Baba temple': 180},
+    'Sprintoor': {'Tennis court': 180, 'Swimming Pool': 54},
+    'Swimming Pool': {'Sprintoor': 54},
+    'Sai Baba temple': {'Tennis court': 180, 'Non IT building': 250},
+    'Colosseum': {'Sai Baba temple': 130},
+    'Short-cut near Non IT building': {'Jain University Gate': 350, 'Non IT building': 120, 'Football field': 240},
+    'Non IT building': {'Short-cut near Non IT building': 120, 'Sai Baba temple': 250}
+}
 
-start_node = 'A'  # Starting node
-end_node = 'E'    # Destination node
+
+start_node = input("START POINT: ")  # Starting node
+end_node = input("DESTINATION: ")    # Destination node
 
     # Run Dijkstra's algorithm
 distances, previous_nodes = dijkstra(graph, start_node)
