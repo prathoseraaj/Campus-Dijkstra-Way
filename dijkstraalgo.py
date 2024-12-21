2cd66ea9291d37bf2d4a49120c506f1d7988ce4b
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-12-21 22:24:16 +0530

    feat: added shortestpath

--- dijkstraalgo.py
+++ dijkstraalgo.py
@@ -25,6 +25,16 @@ def dijkstra(graph, start):
 
     return distances, previous_nodes
 
+def shortest_path(previous_nodes,start,end):
+
+    path = []
+    current = end
+    while current is not None :
+        path.append(current)
+        current = previous_nodes[current]
+    path.reverse()
+
+    return path
 
 
 graph = {
@@ -42,3 +52,7 @@ end_node = 'E'    # Destination node
 distances, previous_nodes = dijkstra(graph, start_node)
 
 print("Distances:", distances)
+print("Previous nodes:", previous_nodes)
+
+print(f"Shortest distance from {start_node} to {end_node}: {distances[end_node]}")
+print(f"Path: {' -> '.join(path)}")
